3f0f349624cc768abb8666f1ddb1ac8d0a47680e
thecsharper <si@thecsharper.com>
2023-12-31 14:39:02 +0000

    Add project files.

---
+++ HttpInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+
+namespace DelegatingHandlerTest
+{
+    public class HttpInterceptor(IOptions<TestOptions> testOptions) : DelegatingHandler
+    {
+        // From: https://timdeschryver.dev/blog/intercepting-http-requests-with-a-delegatinghandler
+        // Example code - non functional
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var bearerToken = GetOptions(testOptions);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token");
+
+            var response = await base.SendAsync(request, cancellationToken);
+            return response;
+        }
+
+        private bool GetOptions(IOptions<TestOptions> testOptions)
+        {
+            return testOptions.Value.Enabled;
+        }
+    }
+
+    public sealed class TestOptions
+    {
+        public bool Enabled { get; set; }
+    }
+}
